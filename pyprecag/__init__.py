206c728015c47766d97a37154616665d09160269
D'Alessandro, Adrian (IM&T, Clayton) <Adrian.D'Alessandro@csiro.au>
2019-01-22 14:18:01 +1100

    Bump version: 0.1.0 → 0.1.1

--- pyprecag/__init__.py
+++ pyprecag/__init__.py
@@ -13,7 +13,7 @@ from . import config
 
 __author__ = 'Christina Ratcliff',
 __email__ = 'Christina.Ratcliff@csiro.au',
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 TEMPDIR = os.path.join(tempfile.gettempdir(), 'PrecisionAg')
 
