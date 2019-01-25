d5536708d66a45d2a12eb0aef4f3341ace437f1f
Ratcliff, Christina (A&F, Waite Campus) <29470818+cratcliff@users.noreply.github.com>
2019-01-25 11:52:05 +1030

    Bump version: 0.1.1 → 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyprecag',
-    version='0.1.1',
+    version='0.2.0',
     description='A suite of tools for Precision Agriculture',
     long_description=long_description,
 
