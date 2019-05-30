8c7cefc45c8a0d35c918781b5d8fcec8ac6d02c2
Christina Ratcliff <29470818+cratcliff@users.noreply.github.com>
2019-05-30 15:37:18 +0930

    #22 Added strip trial analysis using moving window t-test updated create points along line     * added cardinal bearing to strip name     * changed column names 'LineID -> TrialID', 'Transect -> Strip Name'

--- pyprecag/tests/test_convert.py
+++ pyprecag/tests/test_convert.py
@@ -1,11 +1,14 @@
 import shutil
 import unittest
-from geopandas import GeoDataFrame
-from pyprecag.convert import convert_csv_to_points, numeric_pixelsize_to_string
+
+from pyprecag.convert import *
 import time
 import os
 import tempfile
 import logging
+from shapely.geometry import LineString
+
+from pyprecag.tests.test_crs import EPSG_28354_WKT
 
 pyFile = os.path.basename(__file__)
 
@@ -18,11 +21,11 @@ logging.captureWarnings(True)
 logging.basicConfig(level=logging.INFO, format="%(message)s")
 
 
-class test_Convert(unittest.TestCase):
+class TestConvert(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         # 'https://stackoverflow.com/a/34065561'
-        super(test_Convert, cls).setUpClass()
+        super(TestConvert, cls).setUpClass()
         if os.path.exists(TmpDir):
             print 'Folder Exists.. Deleting {}'.format(TmpDir)
             shutil.rmtree(TmpDir)
@@ -47,59 +50,108 @@ class test_Convert(unittest.TestCase):
 
     def run(self, result=None):
         global testFailed
-        unittest.TestCase.run(self, result) # call superclass run method
+        unittest.TestCase.run(self, result)  # call superclass run method
         if len(result.failures) > 0 or len(result.errors) > 0:
-            testFailed=True
+            testFailed = True
+
+    def test_duration(self):
+        start_time = time.time()
+        self.assertEqual(datetime.timedelta(0, 60), duration(start_time, start_time + 60))
+        self.assertEqual(datetime.timedelta(0, 365), duration(start_time, start_time + 365))
 
     def test_convert_csv_to_points_EastingNorthings(self):
-        file = os.path.realpath(this_dir + "/data/area1_yield_ascii_wgs84.csv")
+        in_file = os.path.realpath(this_dir + "/data/area1_yield_ascii_wgs84.csv")
         epsg = 28354
-        out_file = os.path.join(TmpDir, os.path.basename(file).replace('.csv', '_{}.shp'.format(epsg)))
+        out_file = os.path.join(TmpDir,
+                                os.path.basename(in_file).replace('.csv', '_{}.shp'.format(epsg)))
 
-        gdfData, gdfCRS = convert_csv_to_points(file, out_file, coord_columns=['Easting', 'Northing'],
-                                                coord_columns_epsg=epsg)
+        gdf_data, gdf_crs = convert_csv_to_points(in_file, out_file,
+                                                  coord_columns=['Easting', 'Northing'],
+                                                  coord_columns_epsg=epsg)
 
-        self.assertIsInstance(gdfData, GeoDataFrame)
-        self.assertEqual(len(gdfData),13756)
-        self.assertEqual(gdfCRS.epsg_number, epsg)
+        self.assertIsInstance(gdf_data, GeoDataFrame)
+        self.assertEqual(len(gdf_data), 13756)
+        self.assertEqual(gdf_crs.epsg_number, epsg)
         self.assertTrue(os.path.exists(out_file))
 
     def test_convert_csv_to_points_WGS84(self):
-        file = os.path.realpath(this_dir + "/data/area2_yield_ISO-8859-1.csv")
+        in_file = os.path.realpath(this_dir + "/data/area2_yield_ISO-8859-1.csv")
         out_epsg = 28354
-        out_file = os.path.join(TmpDir, os.path.basename(file).replace('.csv', '_{}.shp'.format(out_epsg)))
+        out_file = os.path.join(TmpDir, os.path.basename(in_file).replace(
+            '.csv', '_{}.shp'.format(out_epsg)))
 
-        gdfData, gdfCRS = convert_csv_to_points(file, out_file, coord_columns_epsg=4326, out_epsg=out_epsg)
+        gdf_data, gdf_crs = convert_csv_to_points(in_file, out_file, coord_columns_epsg=4326,
+                                                  out_epsg=out_epsg)
 
         self.assertTrue(os.path.exists(out_file))
-        self.assertIsInstance(gdfData, GeoDataFrame)
-        self.assertEqual(len(gdfData), 1543)
-        self.assertEqual(list(set(gdfData.geom_type)), ['Point'])
+        self.assertIsInstance(gdf_data, GeoDataFrame)
+        self.assertEqual(len(gdf_data), 1543)
+        self.assertEqual(list(set(gdf_data.geom_type)), ['Point'])
 
         import numpy as np
-        np.testing.assert_almost_equal(list(gdfData.total_bounds),
-                               [598868.6709,  6054050.0529,   599242.9049,  6054415.3845], 4)
+        np.testing.assert_almost_equal(list(gdf_data.total_bounds),
+                                       [598868.6709, 6054050.0529, 599242.9049, 6054415.3845], 4)
 
-        self.assertEqual(gdfCRS.epsg_number, out_epsg)
-        self.assertEqual(gdfCRS.crs_wkt,
-                          'PROJCS["GDA94 / MGA zone 54",GEOGCS["GDA94",DATUM["Geocentric_Datum_of_Australia_1994",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6283"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4283"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",141],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",10000000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","28354"]]')
+        self.assertEqual(gdf_crs.epsg_number, out_epsg)
+        self.assertEqual(gdf_crs.crs_wkt, EPSG_28354_WKT)
 
     def test_convert_csv_to_points_WGS84_GuessEPSG(self):
-        file = os.path.realpath(this_dir + "/data/area2_yield_ISO-8859-1.csv")
+        in_file = os.path.realpath(this_dir + "/data/area2_yield_ISO-8859-1.csv")
 
-        out_file = os.path.join(TmpDir, os.path.basename(file).replace('.csv', '_guessepsg.shp'))
-        gdfData, gdfCRS = convert_csv_to_points(file, out_file, coord_columns_epsg=4326, out_epsg=-1)
+        out_file = os.path.join(TmpDir, os.path.basename(in_file).replace('.csv', '_guessepsg.shp'))
+        gdf_data, gdf_crs = convert_csv_to_points(in_file, out_file, coord_columns_epsg=4326,
+                                                  out_epsg=-1)
         self.assertTrue(os.path.exists(out_file))
-        self.assertIsInstance(gdfData, GeoDataFrame)
-        self.assertEqual(len(gdfData), 1543)
-        self.assertEqual(list(set(gdfData.geom_type)), ['Point'])
-        self.assertEqual(gdfCRS.epsg_number, 28354)
-        self.assertEqual(gdfCRS.crs_wkt,
-                          'PROJCS["GDA94 / MGA zone 54",GEOGCS["GDA94",DATUM["Geocentric_Datum_of_Australia_1994",SPHEROID["GRS 1980",6378137,298.257222101,AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6283"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4283"]],PROJECTION["Transverse_Mercator"],PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",141],PARAMETER["scale_factor",0.9996],PARAMETER["false_easting",500000],PARAMETER["false_northing",10000000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","28354"]]')
+        self.assertIsInstance(gdf_data, GeoDataFrame)
+        self.assertEqual(len(gdf_data), 1543)
+        self.assertEqual(list(set(gdf_data.geom_type)), ['Point'])
+        self.assertEqual(gdf_crs.epsg_number, 28354)
+        self.assertEqual(gdf_crs.crs_wkt, EPSG_28354_WKT)
 
     def test_numeric_pixelsize_to_string(self):
         self.assertEqual(numeric_pixelsize_to_string(0.42), '42cm')
         self.assertEqual(numeric_pixelsize_to_string(0.125), '125mm')
         self.assertEqual(numeric_pixelsize_to_string(2.0), '2m')
-        self.assertEqual(numeric_pixelsize_to_string(6000),'6km')
+        self.assertEqual(numeric_pixelsize_to_string(6000), '6km')
         self.assertEqual(numeric_pixelsize_to_string(1500), '1500m')
+
+    def test_cardinal_direction(self):
+        self.assertEqual('E', deg_to_8_compass_pts(89.9999))
+        self.assertEqual('N', deg_to_8_compass_pts(-5))
+        self.assertEqual('SW', deg_to_8_compass_pts(210))
+        self.assertEqual('N', deg_to_8_compass_pts(0.05))
+
+        self.assertEqual('E', deg_to_16_compass_pts(89.9999))
+        self.assertEqual('N', deg_to_16_compass_pts(-5))
+        self.assertEqual('SSW', deg_to_16_compass_pts(210))
+        self.assertEqual('N', deg_to_16_compass_pts(0.05))
+        self.assertEqual('NE', deg_to_16_compass_pts(44))
+        self.assertEqual('NW', deg_to_16_compass_pts(-44))
+
+    def test_pts_linebearing(self):
+        # line_bearing uses point_to_point_bearing so this doubles as it's tests as well
+        c_line_gdf = {'geometry': [LineString([(740873, 6169764), (741269, 6169764)]),
+                                   LineString([(741000, 6169800), (741003, 6170012)]),
+                                   LineString([(741401, 6169800), (741057, 6170012)]),
+                                   LineString([(740900, 6169912), (740979, 6170094)])],
+                      'LineID': [1, 2, 3, 4]}
+
+        c_line_gdf = GeoDataFrame(c_line_gdf, crs={'init': 'epsg:28354'})
+
+        self.assertEqual(90.0, line_bearing(c_line_gdf.iloc[0]))
+        self.assertAlmostEqual(0.8107352192623694, line_bearing(c_line_gdf.iloc[1]), 4)
+        self.assertAlmostEqual(301.64465903709254, line_bearing(c_line_gdf.iloc[2]), 4)
+        self.assertAlmostEqual(23.464022404707464, line_bearing(c_line_gdf.iloc[3]), 4)
+
+    def test_drop_z(self):
+        c_line_gdf = {'geometry': [LineString([(740873, 6169764, 5), (741269, 6169764, 5)]),
+                                   LineString([(741000, 6169800), (741003, 6170012)]),
+                                   LineString([(741401.415, 6169800), (741057, 6170012)]),
+                                   LineString([(740900.861, 6169912), (740979, 6170094)])],
+                      'LineID': [1, 2, 3, 4]}
+        c_line_gdf = GeoDataFrame(c_line_gdf, crs={'init': 'epsg:28354'})
+
+
+
+
+
