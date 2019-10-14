91669ce1a380b46297be90035a335a14d079be09
Andrew Spiers <andrew@andrewspiers.net>
2019-10-14 15:43:19 +1100

    update porting test script
    only create virtualenvs if they don't exist.
    Log python version and 'pip freeze' output.
    Increase timeout
    fix bugs

--- scripts/porting_test.sh
+++ scripts/porting_test.sh
@@ -14,7 +14,7 @@ else
     TARGET_PYTHONS=("python2.7" "python3.6")
 fi
 
-set -x
+#set -x
 
 function check_requirements {
     set -e
@@ -35,7 +35,11 @@ function check_requirements {
 
 function install_gdal {
     _pip=$1
-    "${_pip}" install GDAL=="$(gdal-config --version)" --global-option=build_ext --global-option=-I/usr/include/gdal
+    set -x
+    "${_pip}" install GDAL=="$(gdal-config --version | sed -e 's/$//')" --global-option=build_ext --global-option=-I/usr/include/gdal
+    set +x
+    echo "GDAL installation completed."
+    "${_pip}" freeze
 
 }
 
@@ -46,8 +50,15 @@ function install_pvtools {
 
 
 function run_tests {
-    _python=$1
-    timeout 150s time "${_python}" -m unittest discover -s pyprecag/tests -p "*test*.py" -v
+    _python_version=$1
+    venv_directory=".venv/${_python_version}"
+    _python="${venv_directory}/bin/python"
+    _pip="${venv_directory}/bin/pip"
+    "${_pip}" freeze
+    "${_python}" --version
+    #timeout 150s time "${_python}" -m unittest discover -s pyprecag/tests -p "*test*.py" -v --buffer
+    #timeout 150s time "${_python}" -m unittest discover -s pyprecag/tests -p "*test*.py" --buffer
+    timeout 600s time "${_python}" -m unittest discover -s pyprecag/tests -p "*test*.py" -v
 }
 
 function run_single_tests {
@@ -60,20 +71,45 @@ function run_single_tests {
 
 }
 
+
+function build_virtualenv {
+  # build a virtualenv (and install dependencies) for a given python, if it doesn't already exist.
+  mkdir -p ".venv"
+  PYTHON=$1
+  venv_directory=".venv/${PYTHON}"
+  _pip="${TEMPDIR}/${PYTHON}/bin/pip"
+  echo "testing python virtualenv directory ${venv_directory}"
+  if  test ! -r "${venv_directory}/bin/activate"; then
+      echo virtual environment not found, creating
+      python3 -m virtualenv -p "${PYTHON}" ".venv/${PYTHON}"
+      _pip="${venv_directory}/bin/pip"
+      "${_pip}" install --upgrade pip
+      install_gdal "${_pip}" >/dev/null
+      install_pvtools "${_pip}" >/dev/null
+  fi
+  #${_pip} freeze
+}
+
+
 check_requirements
 
 TEMPDIR=$(mktemp -d)
 
 for PYTHON in "${TARGET_PYTHONS[@]}" ; do
-  python3 -m virtualenv -p "${PYTHON}" "${TEMPDIR}/${PYTHON}"
+  #python3 -m virtualenv -p "${PYTHON}" "${TEMPDIR}/${PYTHON}"
   _pip="${TEMPDIR}/${PYTHON}/bin/pip"
-  ls -l "${_pip}"
-  "${_pip}" --version
-  install_gdal "${_pip}"
-  install_pvtools "${_pip}"
+  # ls -l "${_pip}"
+  #"${_pip}" --version
+  #install_gdal "${_pip}" >/dev/null
+  #install_pvtools "${_pip}" >/dev/null
+
+
   _python="${TEMPDIR}/${PYTHON}/bin/python"
-  run_tests "${_python}"
-  #run_single_tests "${_python}"
+
+  build_virtualenv "${PYTHON}"
+  #run_tests "${_python}"
+  run_tests "${PYTHON}"
+
 done
 
 #find "${TEMPDIR}" -maxdepth 2
