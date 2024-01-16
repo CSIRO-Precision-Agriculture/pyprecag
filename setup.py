9a5802290ce8da63c084b7a68608a673e3582fde
rat062 <29470818+cratcliff@users.noreply.github.com>
2024-01-17 10:17:23 +1030

    update bump2version configuration

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='pyprecag',
-    version='0.4.1',
+    version='0.4.2-dev1',
     description='A suite of tools for Precision Agriculture',
     long_description=long_description,
     long_description_content_type='text/x-rst',
